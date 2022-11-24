f82dba04bc47df09d9c95ff41c2a9feeabc649d6
Guruprasad Kokare <109871757+GuruprasadKokare@users.noreply.github.com>
2022-11-24 13:09:03 +0530

    Add files via upload

---
+++ config1.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+$databaseURL="https://bookcar-38df0-default-rtdb.firebaseio.com/";
---
+++ config2.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+$databaseURL="https://contact-99eb7-default-rtdb.firebaseio.com/";
